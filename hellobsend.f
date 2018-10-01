0cc9b106a381519b6d429e8153be0e785831016d
Evan Shapiro <shapirev@math-compute.ucdenver.pvt>
2018-10-01 14:28:29 -0600

    initial commit

---
+++ hellobsend.f
@@ -0,0 +1,36 @@
+******************************************************************************
+C FILE: mpi_hello.f
+C DESCRIPTION:
+C   MPI tutorial example code: Simple hello world program
+C AUTHOR: Blaise Barney
+C LAST REVISED: 03/05/10
+C ****************************************************************************
+      program hello
+      include 'mpif.h'
+      parameter (MASTER = 0)
+
+      integer numtasks, taskid, len, ierr
+      character(MPI_MAX_PROCESSOR_NAME) hostname
+
+      call MPI_INIT(ierr)
+      call MPI_COMM_SIZE(MPI_COMM_WORLD, numtasks, ierr)
+      call MPI_COMM_RANK(MPI_COMM_WORLD, taskid, ierr)
+
+      call MPI_GET_PROCESSOR_NAME(hostname, len, ierr)
+      write(*,20) taskid, hostname
+      if (taskid .eq. MASTER) then
+        write(*,30) numtasks
+      end if
+
+      if (taskid .lt. numtasks/2) then
+        partner = numtasks/2 + taskid
+      else if (taskid .ge. numtasks/2) then
+        partner = taskid - numtasks/2
+      end if
+
+      call MPI_FINALIZE(ierr)
+
+  20  format('Hello from task ',I2,' on ',A48)
+  30  format('MASTER: Number of MPI tasks is: ',I2)
+
+      end
